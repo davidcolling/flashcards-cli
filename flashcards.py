51f8cf5133039036cd2d138de31d67dc8902aad0
d <d@365922.local>
2021-09-01 17:22:54 -0500

    program now keeps score

--- flashcards.py
+++ flashcards.py
@@ -1,10 +1,22 @@
 import re
+
+number_of_cards = 0
+score = 0
+
 flashcards = open('../cpbox/cards.txt', 'r')
 lines = flashcards.readlines()
 for line in lines:
     if re.match(r'\s', line):
         print(line)
     else:
-        input()
+        if (number_of_cards != 0) :
+            this_score = input("Enter points to add: ")
+            score += int(this_score)
         print(line)
+        number_of_cards += 1
         input()
+
+this_score = input("Enter points to add: ")
+score += int(this_score)
+
+print(str(score) + " / " + str(number_of_cards))
